58a163f2924f2d2db2276b69d1818333a8d6428a
yatharthyadu <yatharth.yadu66@gmail.com>
2025-03-07 01:19:32 +0530

    Added sidebar and navbar with Basic layout

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { ReactNode } from "react";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import { Box } from "@mui/material";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <Box sx={{ display: "flex" }}>
+        <Sidebar />
+        <Box
+          component="main"
+          sx={{ flexGrow: 1, p: 3, ml: "240px", mt: "64px" }}>
+          {children}
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Layout;
---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { AppBar, Toolbar, IconButton, Box } from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <AppBar sx={{ color: "white", backgroundColor: "#fff" }} position="static">
+      <Toolbar>
+        <Box sx={{ flexGrow: 1, display: "flex", alignItems: "center" }}>
+          <Box sx={{ width: 150, height: "auto", position: "relative" }}>
+            <Image
+              src="/assets/logo.svg"
+              alt="GSynergy Logo"
+              width={300}
+              height={120}
+              style={{ width: "100%", height: "auto" }}
+              priority
+            />
+          </Box>
+        </Box>
+
+        {/* Sign In Button */}
+        <IconButton color="inherit">
+          <AccountCircleIcon fontSize="large" sx={{ color: "#000000" }} />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+}
