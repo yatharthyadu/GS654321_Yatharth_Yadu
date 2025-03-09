bd68cc110456e8cd6bfa4b7b0a8b075518e19c19
yatharthyadu <yatharth.yadu66@gmail.com>
2025-03-10 01:59:47 +0530

    UI Cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-
-
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <div className={styles.page}></div>;
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1>Welcome to GS Dashboard</h1>
+      </main>
+    </div>
+  );
 }
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -6,7 +6,13 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <AppBar sx={{ color: "white", backgroundColor: "#fff" }} position="static">
+    <AppBar
+      sx={{
+        color: "white",
+        backgroundColor: "#fff",
+        zIndex: (theme) => theme.zIndex.drawer + 1,
+      }}
+      position="fixed">
       <Toolbar>
         <Box sx={{ flexGrow: 1, display: "flex", alignItems: "center" }}>
           <Box sx={{ width: 150, height: "auto", position: "relative" }}>
@@ -21,7 +27,6 @@ export default function Navbar() {
           </Box>
         </Box>
 
-        {/* Sign In Button */}
         <IconButton color="inherit">
           <AccountCircleIcon fontSize="large" sx={{ color: "#000000" }} />
         </IconButton>
